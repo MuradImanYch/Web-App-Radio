a2653be5c8b950346c654a0403d384c35009ac74
MuradImanYch <mura.imanov@gmail.com>
2025-07-16 07:16:27 +0400

    continue...

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -2,6 +2,70 @@ import Main from "@/components/Main/Main";
 import fallbackStations from "../../../public/assets/docs/mock-api/topclick.json";
 import langJSON from '../../../public/assets/docs/languages.json';
 import { Poppins } from 'next/font/google';
+import conf from '../../../public/assets/docs/conf.json';
+
+export const generateMetadata = ({ params }) => {
+  return {
+    metadataBase: new URL(conf.baseUrl),
+    applicationName: 'Legendary Radio',
+    generator: 'Next.js 14',
+    title: {
+      default: langJSON.translations[langJSON.available.includes(params.lang) ? params.lang : 'en']?.metaTitleMain,
+      template: '%s | Legendary Radio',
+    },
+    description: langJSON.translations[langJSON.available.includes(params.lang) ? params.lang : 'en']?.metaDescMain,
+    keywords: langJSON.translations[langJSON.available.includes(params.lang) ? params.lang : 'en']?.metaKeysMain,
+    alternates: {
+      canonical: langJSON.available.includes(params.lang) ? '/' + params.lang : '/en',
+      languages: { en: '/', ru: '/ru', az: '/az' },
+    },
+    openGraph: {
+      title: langJSON.translations[langJSON.available.includes(params.lang) ? params.lang : 'en']?.metaTitleMain,
+      description: langJSON.translations[langJSON.available.includes(params.lang) ? params.lang : 'en']?.metaOGDescMain,
+      url: conf.baseUrl + langJSON.available.includes(params.lang) ? '/' + params.lang : '/en',
+      siteName: 'Legendary Radio',
+      locale: params.lang + '_' + params.lang.toUpperCase(),
+      type: 'website',
+      images: [
+        {
+          url: '/assets/ico/logo.png',
+          width: 1200,
+          height: 630,
+          alt: langJSON.translations[langJSON.available.includes(params.lang) ? params.lang : 'en']?.metaOGImgAltMain,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: langJSON.translations[langJSON.available.includes(params.lang) ? params.lang : 'en']?.metaTitleMain,
+      description: langJSON.translations[langJSON.available.includes(params.lang) ? params.lang : 'en']?.metaOGDescMain,
+      images: ['/assets/ico/logo.png'],
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        maxSnippet: -1,
+        maxImagePreview: 'large',
+        maxVideoPreview: -1,
+      },
+    },
+    icons: {
+      icon: '/favicon.ico',
+      shortcut: '/favicon.ico',
+      apple: '/favicon.ico',
+    },
+    manifest: '/site.webmanifest',
+    themeColor: '#1b1b21',
+    colorScheme: 'dark light',
+    viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
+    authors: [{ name: 'Imanych', url: 'https://github.com/MuradImanYch' }],
+    creator: 'Imanych',
+    publisher: 'Legendary Radio',
+  }
+};
 
 const poppins = Poppins({
   subsets: ['latin'],
--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,11 @@ import FavLink from "@/components/FavLink/FavLink";
 import LangSwitch from "@/components/LangSwitch/LangSwitch";
 import HeaderLogo from "@/components/HeaderLogo/HeaderLogo";
 import Preloader from "@/components/Preloader/Preloader";
+import Link from "next/link";
 
 export default async function RootLayout({ children }) {
+  const date = new Date();
+
   return (
     <html lang="en">
       <body>
@@ -21,6 +24,11 @@ export default async function RootLayout({ children }) {
           <ClientLayout>
             {children}
           </ClientLayout>
+          <footer>
+            <div className="author">by Imanych</div>
+            <div>© {date.getFullYear()} Legendary Radio</div>
+            <Link className="api-provider" href="https://www.radio-browser.info/" target={'__blank'}>Radio Browser</Link>
+          </footer>
         </Suspense>
       </body>
     </html>
